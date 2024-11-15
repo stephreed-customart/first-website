9d548b5d5016969655cf7c5b6a6bad3386aefa7b
stephreed-customart <stephanie8885@gmail.com>
2024-11-15 10:45:57 -0600

    added Javascript

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*var age = 31;
+var name = 'Lisa';
+console.log(name);
+console.log(age);*/
+
+//This is a single line comment, started with two slashes
+var title = 'Hello';
+/* This is a multi-line comment
+Everything in here has no effect.
+var test = 'This will not actually create a variable';
+*/
+var title2 = 'Hello?'; //Everything after the slashes has no effect
+
+
+console.log("Welcome to my portfolio")
